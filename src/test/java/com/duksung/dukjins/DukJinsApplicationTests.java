eddb540d52c78a339008dcfb78a3fb718e7312d8
chaeyeon <106591445+kchaeys2@users.noreply.github.com>
2023-03-14 17:34:58 +0900

    초기 세팅

---
+++ src/test/java/com/duksung/dukjins/DukJinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duksung.dukjins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DukJinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
