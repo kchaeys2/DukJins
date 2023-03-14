5251a0655082b9f3efe763b081dfc538783d1f38
chaeyeon <106591445+kchaeys2@users.noreply.github.com>
2023-03-14 17:34:58 +0900

    초기 세팅

---
+++ src/main/java/com/duksung/dukjins/DukJinsApplication.java
@@ -0,0 +1,13 @@
+package com.duksung.dukjins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DukJinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DukJinsApplication.class, args);
+	}
+
+}
